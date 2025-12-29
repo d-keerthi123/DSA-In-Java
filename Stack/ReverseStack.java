5777bed78c575939a36e5821352f9cf135a9f389
Danda Keerthi <dandakeerthi2@gmail.com>
2025-12-29 19:54:36 +0530

    Create ReverseStack.java

---
+++ Stack/ReverseStack.java
@@ -0,0 +1,44 @@
+//Time complexity O(n^2)
+//Space complexity O(n) //recursive stack
+
+import java.util.Stack;
+
+public class ReverseStack {
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        //base case
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void reverseStack(Stack<Integer> s){
+        //base case
+        if(s.isEmpty()){
+            return;
+        }
+        int top=s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+
+    }
+    public static void printStack(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s= new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        
+        reverseStack(s);
+        printStack(s); // 1 2 3
+
+    }
+
+}
