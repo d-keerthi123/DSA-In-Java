9b51382bda95d58c7a149965110dd9a09477c5cb
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-20 06:33:58 +0530

    Updated LCA

--- BinaryTree/LCA.java
+++ BinaryTree/LCA.java
@@ -35,22 +35,43 @@ public class LCA {
         path.remove(path.size()-1);
         return false;
     }
+    //Approach 1
+    // public static Node lca(Node root,int n1,int n2){
+    //     ArrayList<Node> path1= new ArrayList<>();
+    //     ArrayList<Node> path2= new ArrayList<>();
+
+    //     getPath(root,n1,path1);
+    //     getPath(root,n2,path2);
+
+    //     int i=0;
+    //     for(;i<path1.size() && i<path2.size();i++){
+    //         if(path1.get(i) != path2.get(i)){
+    //             break;
+    //         }
+    //     }
+    //     //last equal node --> i-1 th node
+    //     Node lca=path1.get(i-1);
+    //     return lca;
+    // }
+
+    //Approach 2
     public static Node lca(Node root,int n1,int n2){
-        ArrayList<Node> path1= new ArrayList<>();
-        ArrayList<Node> path2= new ArrayList<>();
-
-        getPath(root,n1,path1);
-        getPath(root,n2,path2);
-
-        int i=0;
-        for(;i<path1.size() && i<path2.size();i++){
-            if(path1.get(i) != path2.get(i)){
-                break;
-            }
+        //base case
+        if(root == null|| root.data == n1 || root.data ==n2){
+            return root;
         }
-        //last equal node --> i-1 th node
-        Node lca=path1.get(i-1);
-        return lca;
+        Node leftLca=lca(root.left,n1,n2);
+        Node rightLca=lca(root.right,n1,n2);
+
+        if(rightLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rightLca;
+        }
+        
+        return root;
+
     }
     public static void main(String[] args) {
         Node root= new Node(1);
