6c5a2dbb88776fdf7607f134b0bca51918008510
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-22 21:49:20 +0530

    added files

---
+++ BinarySearchTree/BSTSearch.java
@@ -0,0 +1,37 @@
+public class BSTSearch {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static boolean searchBST(Node root,int key){
+        //base case
+        if(root == null){
+            return false;
+        }
+        if(root.data == key){
+            return true;
+        }
+        if(root.data >key){
+           return searchBST(root.left, key);
+        }
+        else{
+            return searchBST(root.right, key);
+        }
+
+        
+        
+    }
+    public static void main(String[] args) {
+        Node root=new Node(8);
+        root.left=new Node(5);
+        root.right=new Node(10);
+        System.out.println(searchBST(root, 6));
+    }
+}
