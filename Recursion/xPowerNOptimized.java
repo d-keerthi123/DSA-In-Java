ea03e1f7c6bfb470e14c9f395016d87c2eaee1ce
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-28 21:48:48 +0530

    Update xPowerNOptimized.java

--- Recursion/xPowerNOptimized.java
+++ Recursion/xPowerNOptimized.java
@@ -3,8 +3,8 @@ public class xPowerNOptimized {
         if(n==0){
             return 1;  //base case 
         }
-        // we might think its optimized but its not stiil we are having time complexity as O(n) only 
-        // bcz we are calling the same func twice so  better we will create a variable for it 
+        // we might think its optimized but its not, still we are having time complexity as O(n) only 
+        // bcz we are calling the same func twice .So  better we will create a variable for it 
         // n even
         // int halfPowerSq=xPnO(x,n/2) * xPnO(x,n/2);
         // if(n%2 != 0){
